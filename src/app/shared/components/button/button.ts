d317bc90e04ff68c6241641d1adf1d92fd543bae
Ray Silva <silvaray355@gmail.com>
2026-07-02 23:26:19 -0300

    feat: add button and modal footer components

---
+++ src/app/shared/components/button/button.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  templateUrl: './button.html',
+  styleUrl: './button.css',
+})
+export class Button {
+  variant = input<'primary' | 'secondary' | 'danger'>('primary');
+  type = input<'button' | 'submit' | 'reset'>('button');
+  disabled = input(false);
+}
