28ae5b1ba41761de18dddce382eb138346a61e88
Ray Silva <silvaray355@gmail.com>
2026-07-03 23:32:41 -0300

    feat: add drag and drop for tasks

--- src/app/pages/kanban-page/components/kanban-board/kanban-board.ts
+++ src/app/pages/kanban-page/components/kanban-board/kanban-board.ts
@@ -4,16 +4,30 @@ import { Modal } from '../../../../shared/components/modal/modal';
 import { Button } from '../../../../shared/components/button/button';
 import { ModalFooter } from '../../../../shared/components/modal-footer/modal-footer';
 import { ModalTask } from "../modal-task/modal-task";
+import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-kanban-board',
-  imports: [KanbanTaskCard, Modal, Button, ModalFooter, ModalTask],
+  imports: [KanbanTaskCard, Modal, Button, ModalFooter, ModalTask, CdkDrag, CdkDropList],
   templateUrl: './kanban-board.html',
   styleUrl: './kanban-board.css',
 })
 export class KanbanBoard {
   protected isModalColumnOpen = signal<boolean>(false);
   protected isModalTaskOpen = signal<boolean>(false);
+  protected tasks = [
+    'Task 1',
+    'Task 2',
+    'Task 3',
+    'Task 4',
+  ]
+
+  protected tasks1 = [
+    'Task 1',
+    'Task 2',
+    'Task 3',
+    'Task 4',
+  ]
 
   protected onDeleteColumn() {
     this.isModalColumnOpen.set(false);
@@ -21,5 +35,18 @@ export class KanbanBoard {
 
   protected onSaveColumn() {
     this.isModalTaskOpen.set(false);
+  }
+
+  protected drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 }
